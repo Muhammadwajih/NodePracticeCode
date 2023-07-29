291455ab0fed0853be7a7e195aa0c42cd005ac5c
Muhammad Wajih Ul Murtaza Sheikh <muhammad.wajih@systemsltd.com>
2023-07-29 14:07:06 +0500

    my first commit

---
+++ Users.js
@@ -0,0 +1,8 @@
+const users = [
+  { id: 1, name: "wajih", email: "wajih@gmail.com" },
+  { id: 2, name: "Asad", email: "asad@gmail.com" },
+  { id: 3, name: "zubair", email: "zubair@gmail.com" },
+  { id: 4, name: "Aqib", email: "aqib@gmail.com" },
+];
+
+module.exports = users;
